a2dbdc66727a0b1522fd898648bb6e027fe91a02
鸿宇 <1527200768@qq.com>
2023-07-27 15:55:03 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hongyukeji/go-utils
+
+go 1.18
---
+++ string/string.go
@@ -0,0 +1,8 @@
+package string
+
+import "os"
+
+// TextTemplateFormat 文本模板格式化 将text字符串中的${example}变量, 通过params参数map数组替换
+func TextTemplateFormat(text string, params map[string]string) string {
+	return os.Expand(text, func(key string) string { return params[key] })
+}
